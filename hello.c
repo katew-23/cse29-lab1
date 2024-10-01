07a92bb4e21ee05160493b3bd525dcdc81c27ee0
katew-23 <kaw068@ucsd.edu>
2024-10-01 22:31:51 +0000

    hello edit

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  char hello[] = "Hello!"; 
+  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+  char hello3[] = "Hi there!";
+
+  puts(hello);
+  puts(hello2);
+
+  puts(hello3);
+
+  return 0;
+}
